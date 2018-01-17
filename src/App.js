852704271eb63e6ee2d6571a0d4e20df2952923d
james sewell <jamesjosephsewell@gmail.com>
2018-01-17 13:18:10 -0600

    added skeleton css boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/icons/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h3 className="App-title">Welcome to React</h3>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code> src/App.js</code> and save to reload.
         </p>
+
+        <div className="test_wrapper">
+          <div className="test_content">
+          test
+          </div>
+
+          <div className="test_content">
+         
+          </div>
+
+        </div>
       </div>
     );
   }
