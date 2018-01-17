22b38803dd2e95be5f6460df413e1c7dc9c87262
james sewell <jamesjosephsewell@gmail.com>
2018-01-17 14:07:49 -0600

    added normalize css to inex.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './images/icons/logo.svg';
 import './App.css';
+import './skeleton.css'
 
 class App extends Component {
   render() {
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h3 className="App-title">Welcome to React</h3>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code> src/App.js</code> and save to reload.
