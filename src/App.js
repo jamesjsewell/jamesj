aa223db996cc46f32e4942d631fdcb6f6d7cbf7a
james sewell <jamesjosephsewell@gmail.com>
2018-01-17 21:13:35 -0600

    updated header

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="row container hero">
+        <header className="row hero">
           <div className="two columns logo_wrapper">
   
               <img className="logo u-max-full-width" src={logo} alt="logo" />  
             
           </div>
-          <div className="ten columns test"><h5 className="header_text">James Sewell - Front End Engineer</h5> <p className="header_text">Houston, TX</p></div>
+          <div className="ten columns test"><h5 className="header_text">James Sewell - Front End Engineer</h5> <p className="subheader_text">Houston, TX</p></div>
         </header>
       </div>
     );
